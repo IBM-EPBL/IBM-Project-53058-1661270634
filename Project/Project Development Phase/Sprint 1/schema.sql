4557b797f09756853d7daccceb018053fbeda7a6
Ashwath19-4001 <113521421+Ashwath19-4001@users.noreply.github.com>
2022-11-19 15:52:39 +0530

    Delete Project/Project Development Phase directory

--- Project/Project Development Phase/Sprint 1/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS user;
-
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL,
-    username TEXT NOT NULL,
-    roll_number INTEGER NOT NULL,
-    pass_word TEXT NOT NULL
-);
